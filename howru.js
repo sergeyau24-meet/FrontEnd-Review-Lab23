0de23c9140e5c54ab1983a0449794d8b00bc4227
sergeyau24-meet <117932841+spookyserge@users.noreply.github.com>
2023-07-17 10:12:22 +0300

    FER sergey

---
+++ howru.js
@@ -0,0 +1,12 @@
+// document.addEventListener('keydown', function(event) {
+//     if(event.keyCode == 37) {
+//         alert('Left was pressed');
+//     }
+//     else if(event.keyCode == 39) {
+//         alert('Right was pressed');
+//     }
+// });
+
+function changecolor(){
+    document.body.style.backgroundColor = "red";
+}
